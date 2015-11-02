36cc2fe3fe68913b2fcd258b3823a742888357f9
BOBBY RANJAN <bbbranjan@gmail.com>
2015-11-02 22:36:00 +0800

    Added Staff and StaffDB updates
    Added Staff and StaffDB updates

--- src/movieapp/Staff.java
+++ src/movieapp/Staff.java
@@ -5,10 +5,58 @@
  */
 package movieapp;
 
+import java.util.*;
+
 /**
  *
  * @author user
  */
-public class Staff {
+public class Staff extends Person{
+    private String cineplex;
+    private int choice;
+    
+    Scanner sc = new Scanner(System.in);
+    public boolean login()
+    {
+        System.out.println("Enter username");
+        
+        System.out.println("Enter password");
+        
+        return true;
+    }
+    
+    public void signup()
+    {
+       System.out.println("Enter Name");
+       System.out.println("Enter username");
+       System.out.println("Enter password");
+       System.out.println("Enter email id");
+       System.out.println("Enter age");
+       System.out.println("Enter phone number");
+       System.out.println("Enter postal code");
+       System.out.println("Enter gender");                 
+    }
+    
+    public int displayMenu()
+    {
+        choice = sc.nextInt();
+        System.out.println("Menu:\n1. List All Movies\n2. Search by name\n3. Search by type\n4. Top 5 by rating\n5. Logout\t\t\t\t6. Exit");
+        switch(choice)
+        {
+            case 1: break;
+        }
+        
+        
+        
+        
+        return choice;
+ 
+    }
+    
+    public void configSettings()
+    {
+        
+    }
+    
     
 }
