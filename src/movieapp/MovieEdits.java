ef21158b115ac380cc0f5d40176074f6aecc3d1a
BOBBY RANJAN <bbbranjan@gmail.com>
2015-11-03 00:34:53 +0800

    MovieEdits.java is born
    MovieEdits.java is born

---
+++ src/movieapp/MovieEdits.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package movieapp;
+
+/**
+ *
+ * @author bobbyranjan
+ */
+public class MovieEdits {
+    
+    public void createMovie(){
+        //will be done later
+    }
+    
+     public void updateMovie(){
+        //will be done later
+    }
+     
+      public void deleteMovie(){
+        //will be done laters
+    }
+      
+       public void updateMovieListing(){
+        //will be done later
+    }
+       
+    
+}
