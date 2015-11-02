2aa6a5adad1d690c2c97f8aacf83c54e7c2ce8ed
BOBBY RANJAN <bbbranjan@gmail.com>
2015-11-02 22:34:53 +0800

    Added Staff and StaffDB updates
    Added Staff and StaffDB updates

--- src/movieapp/StaffDB.java
+++ src/movieapp/StaffDB.java
@@ -5,10 +5,47 @@
  */
 package movieapp;
 
+import java.io.*;
 /**
  *
  * @author user
  */
 public class StaffDB {
+    
+    public boolean readStaff(String user, String pwd) throws IOException, FileNotFoundException{
+    //basic reading code
+        FileInputStream fiStream
+            = new FileInputStream("staffData.dat");
+      BufferedInputStream biStream
+            = new BufferedInputStream(fiStream);
+      ObjectInputStream diStream
+            = new ObjectInputStream(biStream);
+      
+      diStream.close();
+
+    
+return false;
+}
+    
+    public void addStaff(String name, String user, String pwd, String email, int age, String phno, String pcode, char sex) throws IOException, FileNotFoundException{
+      //basic writing code
+      FileOutputStream foStream
+            = new FileOutputStream("staffData.dat");
+      BufferedOutputStream boStream
+            = new BufferedOutputStream(foStream);
+      ObjectOutputStream doStream;
+              
+       doStream = new ObjectOutputStream(boStream);
+
+       doStream.writeUTF(name);
+       doStream.writeInt(age);
+       doStream.writeChar(sex); 
+       
+       doStream.close();
+
+        
+    }
+    
+    
     
 }
