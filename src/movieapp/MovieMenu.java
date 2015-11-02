34f985062d17fbd0317d34cdf79791047e90483e
BOBBY RANJAN <bbbranjan@gmail.com>
2015-11-03 00:19:44 +0800

    MovieMenu class update framework
    MovieMenu class update framework

--- src/movieapp/MovieMenu.java
+++ src/movieapp/MovieMenu.java
@@ -10,5 +10,39 @@ package movieapp;
  * @author user
  */
 public class MovieMenu {
+    private int menuChoice;
+    
+    public void setMenuChoice(int choice)
+    {
+        menuChoice = choice;
+    }
+    
+    public void movieListing()
+    {
+        switch(menuChoice)
+        {
+            case 1: //List all
+                break;
+            case 2: //Search by name
+                break;
+            case 3: //Search by something else
+                break;
+            case 4: //Top 5
+                break;
+            case 5: //View Booking History
+                break;
+            case 6: //Back
+                break;
+            case 7: //Logout
+                break;
+            case 8: //Exit
+                break;
+        }
+    }
+    
+    public void calcPrice(MovieDB movies[])
+    {
+        //will complete later
+    }
     
 }
