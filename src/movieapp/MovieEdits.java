f2e3708d0b5c589350e1b6017174072bf782a360
BOBBY RANJAN <bbbranjan@gmail.com>
2015-11-13 11:10:25 +0800

    MovieEdits
    I’ll delete it later if not required

---
+++ src/movieapp/MovieEdits.java
@@ -0,0 +1,26 @@
+package movieapp;
+
+/**
+ *
+ * @author Team Fork
+ */
+public class MovieEdits {
+    
+    public void createMovie(){
+        //will be done later
+    }
+    
+     public void updateMovie(){
+        //will be done later
+    }
+     
+      public void deleteMovie(){
+        //will be done laters
+    }
+      
+       public void updateMovieListing(){
+        //will be done later
+    }
+       
+    
+}
